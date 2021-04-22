d0c575ff5c4e40fd5215f1fb4e081fb73e1e9e73
stephp23 <stephp23@gmail.com>
2021-04-22 00:20:58 -0400

    updates for find book

--- query.js
+++ query.js
@@ -7,30 +7,50 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 //Find a book
 const findBook = async () => {
-  const thePowerOfNow = await Book.find({ title: 'The Power of Now' })
-  console.log(thePowerOfNow)
+  const findThePowerOfNow = await Book.find( { title:'The Power of Now'} )
+  console.log('Find Book:', findThePowerOfNow)
 }
+
+//Find all books
+const findAllBooks = async () => {
+  const allBooks = await Book.find({})
+  console.log('All Books:', allBooks)
+}
+
+//Create publisher
+const createPublisher = async () => {
+  const eccoPress = await Publisher.insertMany({
+    name: "Ecco",
+    location: "New York",
+    url: "https://www.harpercollins.com/pages/ecco",
+  });
+  console.log("New publisher:", eccoPress)
+}
+
 
 //Creates a book
 const createBook = async () => {
-  const newBook = await Book.create({ title: 'Just As I Am: A Memoir', author: 'Cicely Tyson', published_date: '2021', publisher_id: harperCollins[1]._id })
-  console.log(newBook)
+  const ecco =  await Publisher.find({ name: "Ecco"})
+  const justKids = await Book.insertMany({ title: 'Just Kids', author: 'Patti Smith', published_date: '2010', publisher_id: ecco[0]._id })
+  console.log('New Book:', justKids)
 }
     
 //Updates a book
 const updatesBook = async () => {
   const updatedTheAlchemist = await Book.updateOne({ title: 'The Alchemist' }, { $set: { title: "The Alchemist Revised "} })
-  console.log(updatedTheAlchemist)
+  console.log('Updated Book to Revised Version:', updatedTheAlchemist)
 }
 
 //Deletes a book
 const deletesBook = async () => {
   const deletededZen = await Book.deleteOne({ title: 'Zen and the Art of Motorcycle Maintenance' })
-  console.log(deletededZen)
+  console.log('Deleted Book:', deletededZen)
 }
 
 const run = async () => {
   await findBook()
+  await findAllBooks()
+  await createPublisher()
   await createBook()
   await updatesBook()
   await deletesBook()
